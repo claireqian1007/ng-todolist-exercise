bc88ca493ee00ca9274b9ea54e8e6c3f6c6fbae5
Claire Qian <claire_qian@epam.com>
2020-05-28 16:54:12 +0800

    chore:add list-manager to refactor layer

---
+++ src/app/list-manager/list-manager.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
+
+@Component({
+  selector: 'app-list-manager',
+  templateUrl: './list-manager.component.html',
+  styleUrls: ['./list-manager.component.scss']
+})
+export class ListManagerComponent implements OnInit {
+  todoList: TodoItem[] = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
+  addItem(title: string) {    
+    this.todoList.push({ title });
+  }
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
